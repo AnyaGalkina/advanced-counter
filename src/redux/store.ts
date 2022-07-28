93050fec06fc9fc530e2a23b298dcfec875b59fc
AnyaGalkina <anna.blackbird1@gmail.com>
2022-07-28 14:26:33 +0700

    Install Redux, react-redux +ts. Create store

---
+++ src/redux/store.ts
@@ -0,0 +1,11 @@
+// import {counterReducer} from "./counter-reducer";
+import {combineReducers, legacy_createStore} from "redux";
+
+
+// export type AppStateType = ReturnType<typeof rootReducer>;
+//
+// const rootReducer = combineReducers({
+//     counterState: counterReducer
+// })
+//
+// export let store = legacy_createStore(rootReducer);
