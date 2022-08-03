cd3820bde3df8d00bf7176e4c4726d25543e5ce9
AnyaGalkina <anna.blackbird1@gmail.com>
2022-08-03 22:31:18 +0300

    Add tests for toggle and counter mode. Delete App test

---
+++ src/redux/counter-reducer.test.ts
@@ -0,0 +1,76 @@
+import {
+    counterReducer,
+    InitStateType,
+    setCount, setCounterModeActive,
+    setError,
+    setMaxCountValue,
+    setSettingMode,
+    setStartCountValue, setToggleActive
+} from "./counter-reducer";
+
+let initState: InitStateType;
+
+beforeEach(() => {
+    initState = {
+        startCountValue: 0,
+        maxCountValue: 5,
+        count: 0,
+        settingMode: false,
+        error: false,
+        isToggleActive: false,
+        isCounterModeActive: true,
+    };
+})
+
+test("count should be set", () => {
+    let newState = counterReducer(initState, setCount(3));
+
+    expect(newState.count).toBe(3);
+    expect(initState.count).toBe(0);
+})
+
+test("startCountValue should be updated", () => {
+    let newState = counterReducer(initState, setStartCountValue(3));
+
+    expect(newState.startCountValue).toBe(3);
+    expect(initState.startCountValue).toBe(0);
+})
+
+
+test("maxCountValue should be  updated", () => {
+    let newState = counterReducer(initState, setMaxCountValue(10));
+
+    expect(newState.maxCountValue).toBe(10);
+    expect(initState.maxCountValue).toBe(5);
+})
+
+
+test("settingMode should be changed to true", () => {
+    let newState = counterReducer(initState, setSettingMode(true));
+
+    expect(newState.settingMode).toBeTruthy();
+    expect(initState.settingMode).toBeFalsy();
+})
+
+
+test("error should be changed to true", () => {
+    let newState = counterReducer(initState, setError(true));
+
+    expect(newState.error).toBeTruthy();
+    expect(initState.error).toBeFalsy();
+})
+
+test("isCounterModeActive should be changed to false", () => {
+    let newState = counterReducer(initState, setCounterModeActive(false));
+
+    expect(newState.isCounterModeActive).toBeFalsy();
+    expect(initState.isCounterModeActive).toBeTruthy();
+})
+
+test("isToggleActive should be changed to true", () => {
+    let newState = counterReducer(initState, setToggleActive(true));
+
+    expect(newState.isToggleActive).toBeTruthy();
+    expect(initState.isToggleActive).toBeFalsy();
+})
+
