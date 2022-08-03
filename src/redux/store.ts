aab3e38fd112aadc1966115cb0713831b8558fc5
AnyaGalkina <anna.blackbird1@gmail.com>
2022-08-03 22:21:12 +0300

    Add store and reducers

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,10 @@
-// import {counterReducer} from "./counter-reducer";
+import {counterReducer} from "./counter-reducer";
 import {combineReducers, legacy_createStore} from "redux";
 
+export type AppStateType = ReturnType<typeof rootReducer>;
 
-// export type AppStateType = ReturnType<typeof rootReducer>;
-//
-// const rootReducer = combineReducers({
-//     counterState: counterReducer
-// })
-//
-// export let store = legacy_createStore(rootReducer);
+const rootReducer = combineReducers({
+    counterState: counterReducer
+})
+
+export let store = legacy_createStore(rootReducer);
